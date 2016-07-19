747c47d402e64fce8f8b43c05ea3bc5a91a50180
Michael Wender <michael@michaelwender.com>
2016-07-19 13:35:28 -0400

    Removing content template from front page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,4 @@
 <!-- BEGIN front-page.php -->
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part( 'templates/front-page', 'body' ); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
