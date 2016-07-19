e163bdf83193f780119712f7e629cc3fc3fd07e8
Michael Wender <michael@michaelwender.com>
2016-07-19 13:40:33 -0400

    Adding SVGs

--- templates/front-page-body.php
+++ templates/front-page-body.php
@@ -1,1 +1,24 @@
 <!-- front-page-body.php -->
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="svg circle"><?php get_template_part( 'assets/svg/inline', 'box.svg' ); ?></div>
+			<h3 class="text-center">Donate Your Stuff</h3>
+			<p>Since 2012, PickUpMyDonation.com has been helping you donate your stuff to organizations in your area. Our easy-to-use website, helps you quickly get in touch with donation pick up providers in your area.</p>
+		</div>
+		<div class="col-md-4">
+			<div class="svg circle"><?php get_template_part( 'assets/svg/inline', 'increasing.svg' ); ?></div>
+			<h3 class="text-center">Increase Donations</h3>
+			<p>Are you looking to increase donation pick ups for your organization? Consider these stats:</p>
+			<ul>
+				<li>Last month: <span id="donations-last-month"></span> donations submitted valued at <span id="donations-last-month-value"></span>.</li>
+				<li>This year: <span id="donations-this-year"></span> donations submitted valued at <span id="donations-this-year-value"></span>.</li>
+			</ul>
+		</div>
+		<div class="col-md-4">
+			<div class="svg circle"><?php get_template_part( 'assets/svg/inline', 'checkmark.svg' ); ?></div>
+			<h3 class="text-center">Claim Your Market</h3>
+			<p>Here's a list of our top performing zip codes without an exclusive partner. If your organization provides donaiton pickups in these zip codes, sign up as an exclusive pick up provider to receive all referrals for your zip code(s).</p>
+		</div>
+	</div>
+</div>
