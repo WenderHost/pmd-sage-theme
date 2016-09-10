57f6e53c98b314708fbc4383716f2a8ccbf97ab3
Michael Wender <michael@michaelwender.com>
2016-09-10 14:25:53 -0400

    Adding author archive page header

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,9 @@
 <?php use Roots\Sage\Titles; ?>
 
+<?php if( is_author() ){
+	get_template_part('templates/page-header', 'author');
+} else { ?>
 <div class="page-header">
   <h1><?= Titles\title(); ?></h1>
 </div>
+<?php } ?>
