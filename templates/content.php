4050086dafb050e32dd81e5a386681f4476e1dd3
Michael Wender <michael@michaelwender.com>
2016-09-10 14:22:34 -0400

    Adding post thumbnail, moving entry-meta to bottom

--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,14 @@
 <article <?php post_class(); ?>>
+  <?php
+  if( has_post_thumbnail() ){
+  	the_post_thumbnail( 'feature-thumbnail', ['class'=>'alignright feature-thumbnail'] );
+  }
+  ?>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
+  <div class="entry-summary lead">
     <?php the_excerpt(); ?>
   </div>
+  <?php get_template_part('templates/entry-meta'); ?>
 </article>
