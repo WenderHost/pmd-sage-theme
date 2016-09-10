6929ef25df476218adf4c05c1ded1b6c68f29997
Michael Wender <michael@michaelwender.com>
2016-09-10 14:16:13 -0400

    Adding feature-thumbnail size

--- lib/setup.php
+++ lib/setup.php
@@ -36,6 +36,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
+  add_image_size( 'feature-thumbnail', 512, 512, true );
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
