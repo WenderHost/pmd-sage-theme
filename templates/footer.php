bb12e1369e87fa9846aa2e19ce6c95f73d842142
Michael Wender <michael@michaelwender.com>
2016-09-15 14:13:06 -0400

    Adding footer navigation

--- templates/footer.php
+++ templates/footer.php
@@ -48,7 +48,12 @@
 	<div class="jumbotron ribbon">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12">&copy; <?php echo date( 'Y' ) ?> <?php bloginfo( 'title' ); ?>. All rights reserved.</div>
+				<div class="col-md-8"><?php
+				if( has_nav_menu( 'footer_navigation' ) ){
+					wp_nav_menu( ['theme_location' => 'footer_navigation']);
+				}
+				?></div>
+				<div class="col-md-4 copyright">&copy; <?php echo date( 'Y' ) ?> <?php bloginfo( 'title' ); ?>. All rights reserved.</div>
 			</div>
 		</div>
 	</div><!-- .jumbotron.ribbon -->
