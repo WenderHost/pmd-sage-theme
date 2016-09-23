1985919ccfa3883f13081c3b6debd20c225734ba
Michael Wender <michael@michaelwender.com>
2016-09-23 15:32:23 -0400

    Conditional checks for CountUps

--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -7,21 +7,32 @@
 			separator: ','
 		};
 
-		var totalDonations = new CountUp('total-donations', 0, data.donations.alltime.number, 0, 2, options );
-		totalDonations.start();
-		var totalDonationsVal = new CountUp('total-donations-value', 0, data.donations.alltime.value, 0, 2.5, {separator: ',', prefix: '$'} );
-		totalDonationsVal.start();
+		if($('#total-donations').length > 0){
+			var totalDonations = new CountUp('total-donations', 0, data.donations.alltime.number, 0, 2, options );
+			totalDonations.start();
+		}
 
-		var donationsLastMonth = new CountUp('donations-last-month', 0, data.donations.lastmonth.number, 0, 2, options );
-		var donationsLastMonthVal = new CountUp('donations-last-month-value', 0, data.donations.lastmonth.value, 0, 2, {separator: ',', prefix: '$'} );
+		if($('#total-donations-value').length > 0){
+			var totalDonationsVal = new CountUp('total-donations-value', 0, data.donations.alltime.value, 0, 2.5, {separator: ',', prefix: '$'} );
+			totalDonationsVal.start();
+		}
 
-		var waypoints = $('#partner-cta').waypoint({
-		  handler: function(direction) {
-		    console.log(this.element.id + ' hit');
-		    donationsLastMonth.start();
-		    donationsLastMonthVal.start();
-		  }
-		});
+		if($('#donations-last-month').length >0){
+			var donationsLastMonth = new CountUp('donations-last-month', 0, data.donations.lastmonth.number, 0, 2, options );
+		}
+		if($('#donations-last-month-value').length >0){
+			var donationsLastMonthVal = new CountUp('donations-last-month-value', 0, data.donations.lastmonth.value, 0, 2, {separator: ',', prefix: '$'} );
+		}
+
+		if($('#donations-last-month').length >0){
+			var waypoints = $('#partner-cta').waypoint({
+			  offset: '50%',
+			  handler: function(direction) {
+			    donationsLastMonth.start();
+			    donationsLastMonthVal.start();
+			  }
+			});
+		}
 
 	});
 
