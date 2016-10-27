ee38faafe8fb994f6bb6f49691d2b5818f770acf
Michael Wender <michael@michaelwender.com>
2016-10-27 16:43:40 -0400

    Responsive styling and layout adjustments for mobile

--- templates/content.php
+++ templates/content.php
@@ -1,12 +1,12 @@
 <article <?php post_class(); ?>>
+  <header>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  </header>
   <?php
   if (has_post_thumbnail()) {
     the_post_thumbnail('feature-thumbnail', ['class' => 'alignright feature-thumbnail']);
   }
   ?>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </header>
   <div class="entry-summary lead">
     <?php the_excerpt(); ?>
   </div>
