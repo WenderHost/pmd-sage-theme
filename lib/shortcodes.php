2dd2dae6db0a4793bbf54bc848dbd3c59d71c894
Michael Wender <michael@michaelwender.com>
2017-01-17 09:56:01 -0500

    Adding [load content=""] shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -9,7 +9,7 @@ function theme_logo( $atts ){
             'style' => '',
         ], $atts );
 
-    $logo = \get_stylesheet_directory_uri() . '\dist\images\logo.1505x180.png';
+    $logo = \get_stylesheet_directory_uri() . '/dist/images/logo.1505x180.png';
 
     $height = intval( ($args['width']*180)/1505 );
 
@@ -18,3 +18,29 @@ function theme_logo( $atts ){
     return $html;
 }
 add_shortcode( 'logo', __NAMESPACE__ . '\\theme_logo' );
+
+/**
+ * Loads a /template/ file.
+ *
+ * @param      array  $atts   Specify the file to be loaded as $atts['content']
+ *
+ * @return     string  Contents of loaded file with 'the_content' filters applied
+ */
+function load_content( $atts ){
+    $args = \shortcode_atts( [
+        'content' => null,
+    ], $atts );
+
+    if( is_null( $args['content'] ) )
+        return '<div class="alert alert-error">Please specify your content via the `content` attribute.</div>';
+
+    $file = \get_stylesheet_directory() . '/templates/' . $args['content'] . '.php';
+    if( ! file_exists( $file ) )
+        return '<div class="alert alert-error">File (<code>' . basename( $file ) . '</code>) not found!</div>';
+
+    ob_start();
+    include( $file );
+    $file_contents = ob_get_clean();\
+    return $file_contents;
+}
+add_shortcode( 'load', __NAMESPACE__ . '\\load_content' );
