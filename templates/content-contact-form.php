cb4fdf377c5f0ce81e3a22ea53ed09ccf52f5f8c
Michael Wender <michael@michaelwender.com>
2021-04-19 14:47:54 -0400

    Adding contact form template

---
+++ templates/content-contact-form.php
@@ -0,0 +1,15 @@
+<div class="container-fluid" id="partner-cta">
+    <div class="jumbotron nomargin jumbotron-cta">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Get In Touch With Us</h2>
+                    <p>Contact us today to discuss adding your organization to our provider network:</p>
+                </div>
+                <div class="col-md-6">
+                    <?php gravity_form( 16 , false, false, false, null, true, 99 ) ?>
+                </div>
+            </div><!-- .row -->
+        </div>
+    </div>
+</div>
